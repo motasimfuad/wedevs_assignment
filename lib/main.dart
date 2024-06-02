0855abaea91f6754f0e34b66683edf69d4000478
Motasim Fuad <motasimfuad.dev@gmail.com>
2024-06-02 16:48:15 +0600

    chore(#5): GetX Added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:motasimfuad_wedevs/src/features/authentication/sign_in/views/sign_in_screen.dart';
 
 void main() {
@@ -8,13 +9,14 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       title: 'Dokan',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.deepPurple,
+        ),
         useMaterial3: true,
       ),
       home: const SignInScreen(),
